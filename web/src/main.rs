a6b10eca502c4d19eefedb3b38832676c45fefa7
Genna Wingert <wingertge@gmail.com>
2020-08-12 10:43:46 +0200

    Add HTML template scaffolding

--- web/src/main.rs
+++ web/src/main.rs
@@ -7,16 +7,29 @@ use api_types::user::user_client::UserClient;
 use rocket::State;
 use tonic::Request;
 use api_types::user::SayRequest;
+use crate::templates::index_html;
+use rocket::response::content::Html;
 
+mod templates;
+
+type Template = Html<Vec<u8>>;
 type UserAPI<'a> = State<'a, UserClient<Channel>>;
 
+fn write_html<F: FnOnce(&mut Vec<u8>)>(f: F) -> Template {
+    let mut buf = Vec::new();
+    f(&mut buf);
+    Html(buf)
+}
+
 #[get("/")]
-async fn index(api: UserAPI<'_>) -> String {
+async fn index(api: UserAPI<'_>) -> Template {
     let request = Request::new(SayRequest {
         name: "Lisa".to_string()
     });
     let response = api.clone().send(request).await.unwrap().into_inner();
-    response.message
+    write_html(|out| {
+        index_html(out, &response.message, &["test item"]).unwrap()
+    })
 }
 
 #[tokio::main]
---
+++ web/src/templates/template_index_html.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Write};
+#[allow(renamed_and_removed_lints)]
+#[cfg_attr(feature="cargo-clippy", allow(useless_attribute))]
+#[allow(unused)]
+use super::{Html,ToHtml};
+
+pub fn index_html<W>(mut _ructe_out_: &mut W, name: &str, items: &[&str]) -> io::Result<()> where W: ?Sized, for<'a> &'a mut W: Write {
+_ructe_out_.write_all(b"<html lang=\"en\">\r\n    <head><title>")?;
+name.to_html(&mut _ructe_out_)?;
+_ructe_out_.write_all(b"</title></head>\r\n        <body>\r\n            ")?;
+if items.is_empty() {
+_ructe_out_.write_all(b"\r\n                <p>There are no items.</p>\r\n            ")?;
+} else {
+_ructe_out_.write_all(b"\r\n                <p>There are ")?;
+items.len().to_html(&mut _ructe_out_)?;
+_ructe_out_.write_all(b" items.</p>\r\n                <ul>\r\n                    ")?;
+for item in items {
+_ructe_out_.write_all(b"\r\n                        <li>")?;
+item.to_html(&mut _ructe_out_)?;
+_ructe_out_.write_all(b"</li>\r\n                    ")?;
+}
+_ructe_out_.write_all(b"</ul>\r\n            ")?;
+}
+_ructe_out_.write_all(b"\r\n        </body>\r\n</html>")?;
+Ok(())
+}
