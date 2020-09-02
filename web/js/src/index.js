7c68467a9d8612f5e30497acb3c693d2f6791dd4
Spencer Summerville <me@grenuttag.com>
2020-09-02 10:57:05 -0400

    Add Snowpack dependencies and hook up app to web API

--- web/js/src/index.js
+++
@@ -1,4 +0,0 @@
-import App from "./components/App"
-import "./styles/main.css"
-
-export default App
---
+++ web/js/src/index.jsx
@@ -0,0 +1,6 @@
+import { h, render } from "preact"
+import App from "./components/App"
+
+import "./styles/main.css"
+
+render(<App />, document.getElementById("root"))
